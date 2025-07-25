26eb83835fc58816cc0f08edb34333b76f8aac65
Lena Nichols <lena.k.nichols@gmail.com>
2025-07-25 09:07:03 -0700

    Initial Commit

---
+++ Birthdays app/Birthdays_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  Birthdays_appApp.swift
+//  Birthdays app
+//
+//  Created by Scholar on 7/25/25.
+//
+
+import SwiftUI
+
+@main
+struct Birthdays_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
