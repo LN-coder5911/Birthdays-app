9c39f6b877f91aac70e0838f0a0d64765b3d3894
Lena Nichols <lena.k.nichols@gmail.com>
2025-07-25 10:25:06 -0700

    created root veiw

--- Birthdays app/ContentView.swift
+++ Birthdays app/ContentView.swift
@@ -8,16 +8,22 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var friends : [Friend] = [Friend(name:"Lennon Nichols", birthday: .now), Friend(name: "Lena", birthday: .now)]
+    
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
-    }
-}
+        NavigationStack{
+            List(friends, id: \.name) { friend in
+                HStack{
+                    Text(friend.name)
+                    Spacer()
+                    Text(friend.birthday, format: .dateTime.month(.wide).day().year())
+                } //hstack
+            } //list
+            .navigationTitle("Birthdays")
+        } //nav
+    } //body
+} //struct
 
 #Preview {
     ContentView()
